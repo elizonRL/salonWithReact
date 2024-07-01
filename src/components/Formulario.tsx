e0dde1b6b632185912463d610f8b11bcc5032ca5
Elizon Rodrigues <118481749+elizonRL@users.noreply.github.com>
2024-07-01 09:49:43 -0400

    funtionality of form

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,40 +1,44 @@
+import { useForm } from "react-form-ease";
 import "../styles/form.css";
+
 export default function Formulario() {
+  const {formData, updateForm}= useForm({
+    data:{
+      name: '', 
+      email:'',
+      date: '',
+      hour: ''
+    }
+  })
   return (
     <>
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        // Lógica para enviar el formulario
-        const data = {
-           name: e.currentTarget.nameR.value,
-           email: e.currentTarget.email.value,
-           date: e.currentTarget.date.value,
-           hour: e.currentTarget.hour.value
-        };
-        console.log(data);
-      }} className="form">
+      <form  method="post"  className="form">
         <div className="form-input">
           <div>
             <label htmlFor="name">Nombre</label>
-            <input type="text" name="nameR" />
+            <input type="text" name="name" onChange={(e)=>updateForm({name: e.target.value}) }/>
           </div>
           <div>
             <label htmlFor="">Email</label>
-            <input type="email" name="email" />
+            <input type="email" name="email" onChange={(e)=>updateForm({email: e.target.value}) } />
           </div>
         </div>
         <div className="form-input">
           <div>
             <label htmlFor="">Fecha</label>
-            <input type="date" name="date" />
+            <input type="date" name="date" onChange={(e) => updateForm({date: e.target.value })}/>
           </div>
           <div>
             <label htmlFor="">Hour</label>
-            <input type="time" min={"9:00"} max={"20:00"} required name="hour" />
+            <input type="time" min={"9:00"} max={"20:00"} required name="hour" onChange={(e)=> updateForm({hour: e.target.value})} />
           </div>
         </div>
         <button type="submit">Enviar</button>
       </form>
+      <p>{formData.name}</p>
+      <p>{formData.email}</p>
+      <p>{formData.date}</p>
+      <p>{formData.hour}</p>
     </>
   );
 }
